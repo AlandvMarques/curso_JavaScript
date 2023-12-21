a0bf8f1b6877315245884aa749a8cea46558fc9c
AlandvMarques <137972339+AlandvMarques@users.noreply.github.com>
2023-12-21 16:10:52 -0300

    alteração do X005.js

--- EXERCICIOS/Exercicios5/X005.js
+++ EXERCICIOS/Exercicios5/X005.js
@@ -1,24 +1,17 @@
-// expressão inicial; expressão condicional; finalização;
+expressão inicial; expressão condicional; finalização;
 
-// //teste 1:
-// indice = 0;
+//teste 1:
+indice = 0;
 
-// for (
-//     alert("Inicializando for");
-//     indice <= 10;
-//     alert("Finalizando um bloco de repetição")
-// ){
-//     alert("indice = " + indice)
-//     indice++
-// }
-
-
-
-let name = "hello, World"
-
-for(let indece = 0; indece < name.length; indece++){
-    console.log(name [indece])
+for (
+    alert("Inicializando for");
+    indice <= 10;
+    alert("Finalizando um bloco de repetição")
+){
+    alert("indice = " + indice)
+    indice++
 }
+
 
 
 
